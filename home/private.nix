5a99b79a600ae96c5a43d92cad8a608739ac1b71
Daniel Nägele <daniel@naegele.dev>
2025-05-13 23:38:38 +0200

    add proper mimetypes for firefox

--- home/private.nix
+++ home/private.nix
@@ -192,6 +192,11 @@ in
         "video/mp4" = [ "vlc.desktop" ];
         "video/quicktime" = [ "vlc.desktop" ];
         "application/wps-office.pptx" = [ "impress.desktop" ];
+        "text/html" = [ "firefox.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/about" = [ "firefox.desktop" ];
+        "x-scheme-handler/unknown" = [ "firefox.desktop" ];
       };
     };
   };
